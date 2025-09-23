cba7be3d0b3c3adcb51169d6f85db171a5ce45ce
0285753-user <0285753@up.edu.mx>
2025-09-22 20:05:00 -0600

    Create header.py

---
+++ Modules/UI/header.py
@@ -0,0 +1,18 @@
+# up_bi_2025_Sept/Modules/Utils/headr.py
+#Encabezado de mi interfaz grafica de mi app
+
+import streamlit as st
+
+def show_header(text_title: tr):
+  #layout: logo + title side by side
+  col1, col2 = st.columns([1, 6)]
+
+  with col1:
+    st.image("assets/UP logo.jpg", width=200)
+  
+  with col2:
+    st.title(text_title)
+    st.caption(" Developed for: *Business Intelligence (Graduate Level)*")
+    st caption("Instructor: Edgar Avalos-Ganua (2025), Universidad Panamericana")
+
+  
